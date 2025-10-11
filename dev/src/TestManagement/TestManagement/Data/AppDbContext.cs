a5855a5cfe1e852701fbd50348cfc155703c4091
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-11 22:02:35 +0900

    Add data base context class.

---
+++ dev/src/TestManagement/TestManagement/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestManagement.Models;
+
+namespace TestManagement.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TestCase> TestCases { get; set; }
+        public DbSet<TestRun> TestRuns { get; set; }
+        public DbSet<XmlImportHistory> XmlImportHistories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
