8f4fcfaa35f97559ae276848a7f29c57085593a3
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-11 22:14:12 +0900

    Update TestCase.cs file. Remove TestRun and XmlImportHistory class, declared in other file and duplicated classes.

--- dev/src/TestManagement/TestManagement/Models/TestCase.cs
+++ dev/src/TestManagement/TestManagement/Models/TestCase.cs
@@ -9,24 +9,4 @@
 
         public ICollection<TestRun> TestRuns { get; set; } = new List<TestRun>();
     }
-
-    public class TestRun
-    {
-        public int TestRunId { get; set; }
-        public int TestCaseId { get; set; }
-        public string Status { get; set; } = string.Empty;
-        public DateTime ExecutedAt { get; set; }
-        public string Log { get; set; } = string.Empty;
-
-        public TestCase TestCase { get; set; } = new TestCase();
-    }
-
-    public class XmlImportHistory
-    {
-        public int ImportId { get; set; }
-        public string FileName { get; set; } = string.Empty;
-        public DateTime ImportedAt { get; set; }
-        public string Status { get; set; } = string.Empty;
-        public string ErrorMessage { get; set; } = string.Empty;
-    }
 }
