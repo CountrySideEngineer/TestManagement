ea9fdc91ccdab15ebb5362177fb6f18ddcf73611
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-12 09:32:44 +0900

    Update XmlImportHistory class. Add "Key" attribute to ImportId property to describe it is primary key of the table.

--- dev/src/TestManagement/TestManagement/Models/XmlImportHistory.cs
+++ dev/src/TestManagement/TestManagement/Models/XmlImportHistory.cs
@@ -1,7 +1,10 @@
-﻿namespace TestManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestManagement.Models
 {
     public class XmlImportHistory
     {
+        [Key]
         public int ImportId { get; set; }
         public string FileName { get; set; } = string.Empty;
         public DateTime ImportedAt { get; set; }
