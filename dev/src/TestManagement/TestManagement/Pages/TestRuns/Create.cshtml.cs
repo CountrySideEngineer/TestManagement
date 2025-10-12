25df6657610777725b7d5022de8c1d5225144a32
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-13 07:22:52 +0900

    Add TestRuns page files.

---
+++ dev/src/TestManagement/TestManagement/Pages/TestRuns/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TestManagement.Data;
+using TestManagement.Models;
+
+namespace TestManagement.Pages.TestRuns
+{
+    public class CreateModel : PageModel
+    {
+        private readonly TestManagement.Data.AppDbContext _context;
+
+        public CreateModel(TestManagement.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["TestCaseId"] = new SelectList(_context.TestCases, "Id", "Name");
+            return Page();
+        }
+
+        [BindProperty]
+        public TestRun TestRun { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.TestRuns.Add(TestRun);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
