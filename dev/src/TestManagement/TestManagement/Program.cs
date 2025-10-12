f5c110dc65ce817332c87f2fc69963096f03248b
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-13 07:23:46 +0900

    Update Program.cs. Add codes to map controllers and throw exceptions.

--- dev/src/TestManagement/TestManagement/Program.cs
+++ dev/src/TestManagement/TestManagement/Program.cs
@@ -17,6 +17,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -25,6 +29,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapControllers(); 
 app.MapRazorPages();
 
 app.Run();
