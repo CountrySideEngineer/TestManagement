2e8956ee7a2fe53ffbb8e9744262e87590dfc31a
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-13 09:25:32 +0900

    Add page to register test case.

---
+++ dev/src/TestManagement/TestManagement/Pages/TestCases/Create.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TestManagement.Models;
+
+namespace TestManagement.Pages.TestCases
+{
+    public class CreateModel : PageModel
+    {
+        private readonly TestManagement.Data.AppDbContext _context;
+
+        public CreateModel(TestManagement.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public TestCase TestCase { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // 作成日時をUTCで設定
+            TestCase.CreatedAt = DateTime.UtcNow;
+
+            _context.TestCases.Add(TestCase);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
