2e19f8dba1c53cc29debb7a2ce4a50eb01b37865
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-18 22:30:02 +0900

    Add new page of test case model. Add page to display details of test case.

---
+++ dev/src/TestManagement/TestManagement/Pages/TestCases/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TestManagement.Data;
+using TestManagement.Models;
+
+namespace TestManagement.Pages.TestCases
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly TestManagement.Data.AppDbContext _context;
+
+        public DetailsModel(TestManagement.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public TestCase TestCase { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var testcase = await _context.TestCases.FirstOrDefaultAsync(m => m.Id == id);
+            if (testcase == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                TestCase = testcase;
+            }
+            return Page();
+        }
+    }
+}
