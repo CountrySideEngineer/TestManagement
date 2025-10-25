8af2693117fc4414b6bc919f4a7e76c7b8d5d228
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-25 10:54:34 +0900

    Add TestManagementDbContext class. Add a database context class for the application.

---
+++ dev/src/TestManagement/TestManagement/Data/TestManagementDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestManagement.Data
+{
+    public class TestManagementDbContext : DbContext
+    {
+        public TestManagementDbContext(DbContextOptions<TestManagementDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
