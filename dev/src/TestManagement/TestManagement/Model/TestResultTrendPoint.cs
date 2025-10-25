42c018bb6941b369e875085bf369e494a14c30f8
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-25 10:43:51 +0900

    Add TestResultTrendPoint class.

---
+++ dev/src/TestManagement/TestManagement/Model/TestResultTrendPoint.cs
@@ -0,0 +1,10 @@
+﻿namespace TestManagement.Model
+{
+    public class TestResultTrendPoint
+    {
+        public DateTime Date { get; set; }
+        public int SuccessCount { get; set; }
+        public int FailureCount { get; set; }
+        public int SkippedCount { get; set; }
+    }
+}
