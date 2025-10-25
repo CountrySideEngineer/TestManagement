7dbd1d2e9f55d5913ec67fb42572644bddba9031
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-25 10:38:09 +0900

    Add TestResultStatistics class.

---
+++ dev/src/TestManagement/TestManagement/Model/TestResultStatistics.cs
@@ -0,0 +1,12 @@
+﻿namespace TestManagement.Model
+{
+    public class TestResultStatistics
+    {
+        public int SuccessCount { get; set; }
+        public int FailureCount { get; set; }
+        public int SkippedCount { get; set; }
+        public double SuccessRate => (SuccessCount + FailureCount + SkippedCount) == 0
+            ? 0
+            : 100.0 * SuccessCount / (SuccessCount + FailureCount + SkippedCount);
+    }
+}
