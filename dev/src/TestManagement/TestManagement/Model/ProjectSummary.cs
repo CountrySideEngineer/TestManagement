b5372e0575a5af6a2e8aa9047f768a9fcb271d03
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-25 10:19:46 +0900

    Add ProjectSummary class. Add a model class about project.

---
+++ dev/src/TestManagement/TestManagement/Model/ProjectSummary.cs
@@ -0,0 +1,12 @@
+﻿namespace TestManagement.Model
+{
+    public class ProjectSummary
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; } = "";
+        public int TotalCases { get; set; }
+        public int ExecutedCases { get; set; }
+        public double SuccessRate { get; set; }
+        public DateTime? LastExecutedAt { get; set; }
+    }
+}
