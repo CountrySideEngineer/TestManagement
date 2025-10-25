e222bb5346cf71ebcf598c2740f38702434e6bcd
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-25 10:29:18 +0900

    Add TestRunSummary class.

---
+++ dev/src/TestManagement/TestManagement/Model/TestRunSummary.cs
@@ -0,0 +1,11 @@
+﻿namespace TestManagement.Model
+{
+    public class TestRunSummary
+    {
+        public int RunId { get; set; }
+        public DateTime ExecutedAt { get; set; }
+        public string Executor { get; set; } = "";
+        public string CaseName { get; set; } = "";
+        public string Result { get; set; } = "";
+    }
+}
