93a12b1fe23cdf81a073caaa2b0771f66fcbc44a
CountrySideEngineer <deep-sleeper@nifty.com>
2025-10-25 10:49:25 +0900

    Add DeveloperDashboardViewModel view model class.

---
+++ dev/src/TestManagement/TestManagement/Model/DeveloperDashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TestManagement.Model
+{
+    public class DeveloperDashboardViewModel
+    {
+        public ProjectSummary Summary { get; set; } = new ProjectSummary();
+        public List<TestRunSummary> RecentRuns { get; set; } = new();
+        public TestResultStatistics Statistics { get; set; } = new TestResultStatistics();
+        public List<TestResultTrendPoint> Trend { get; set; } = new();
+    }
+}
