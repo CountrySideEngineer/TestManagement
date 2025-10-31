e7224e938a0252344367f1f2d7b88fa4bbb6fef8
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-01 06:25:53 +0900

    Update db context. Add relation ship among models.

--- dev/src/TestManagement/TestManagement/Data/TestManagementDbContext.cs
+++ dev/src/TestManagement/TestManagement/Data/TestManagementDbContext.cs
@@ -21,6 +21,48 @@ namespace TestManagement.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Project>()
+                        .HasMany(p => p.TestSuites)
+                        .WithOne(s => s.Project)
+                        .HasForeignKey(s => s.ProjectId)
+                        .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Project>()
+                .HasMany(p => p.TestRuns)
+                .WithOne(r => r.Project)
+                .HasForeignKey(r => r.ProjectId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TestSuite>()
+                .HasMany(s => s.TestCases)
+                .WithOne(c => c.TestSuite)
+                .HasForeignKey(c => c.TestSuiteId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TestLevel>()
+                .HasMany(l => l.TestCases)
+                .WithOne(c => c.TestLevel)
+                .HasForeignKey(c => c.TestLevelId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<TestCase>()
+                .HasMany(c => c.TestResults)
+                .WithOne(rr => rr.TestCase)
+                .HasForeignKey(rr => rr.TestCaseId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TestRun>()
+                .HasMany(r => r.TestResults)
+                .WithOne(rr => rr.TestRun)
+                .HasForeignKey(rr => rr.TestRunId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Tester>()
+                .HasMany(t => t.TestRuns)
+                .WithOne(r => r.Tester)
+                .HasForeignKey(r => r.TesterId)
+                .OnDelete(DeleteBehavior.SetNull);
         }
     }
 }
