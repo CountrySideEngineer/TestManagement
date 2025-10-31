ff7a1f1db321b8653f9d4d5d0f7d5182e15a2bcb
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-01 06:19:50 +0900

    Update DbContext class.

--- dev/src/TestManagement/TestManagement/Data/TestManagementDbContext.cs
+++ dev/src/TestManagement/TestManagement/Data/TestManagementDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TestManagement.Model;
 
 namespace TestManagement.Data
 {
@@ -7,6 +8,19 @@ namespace TestManagement.Data
         public TestManagementDbContext(DbContextOptions<TestManagementDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Project> Projects => Set<Project>();
+        public DbSet<TestSuite> TestSuites => Set<TestSuite>();
+        public DbSet<TestLevel> TestLevels => Set<TestLevel>();
+        public DbSet<TestCase> TestCases => Set<TestCase>();
+        public DbSet<Tester> Testers => Set<Tester>();
+        public DbSet<TestRun> TestRuns => Set<TestRun>();
+        public DbSet<TestResult> TestResults => Set<TestResult>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
