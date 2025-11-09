b859ef580f5cc0402ba108a3e397295cc0c91729
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-09 21:17:33 +0900

    Add ProjectController class. Add the class to confirm the operation about project controller.

---
+++ dev/src/TestManagement/TestManagement/Controllers/ProjectController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestManagement.Model;
+using TestManagement.Serivces;
+
+namespace TestManagement.Controllers
+{
+    [Route("api/[conttroller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly ProjectService _projectService;
+
+        public ProjectController(ProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProjects()
+        {
+            var projects = await _projectService.GetAllAsync();
+            return Ok(projects);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var project = await _projectService.GetProjectWithSuiteAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return Ok(project);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Project project)
+        {
+            await _projectService.AddAsync(project);
+            return CreatedAtAction(nameof(Get), new { id = project.Id }, project);
+        }
+    }
+}
