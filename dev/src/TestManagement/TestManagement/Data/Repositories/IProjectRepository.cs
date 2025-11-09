44d54a636a791b2be7ce3bbf20af29c2a8f9be67
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-09 17:41:47 +0900

    Add repository interface and class. Add IProjectRepository as interface and ProjectRepository class.

---
+++ dev/src/TestManagement/TestManagement/Data/Repositories/IProjectRepository.cs
@@ -0,0 +1,9 @@
+﻿using TestManagement.Model;
+
+namespace TestManagement.Data.Repositories
+{
+    public interface IProjectRepository : IGenericRepository<Project>
+    {
+        Task<Project?> GetProjectWithSuitesAsync(int projectId);
+    }
+}
---
+++ dev/src/TestManagement/TestManagement/Data/Repositories/ProjectRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestManagement.Model;
+
+namespace TestManagement.Data.Repositories
+{
+    public class ProjectRepository : GenericRepository<Project>, IProjectRepository
+    {
+        private readonly TestManagementDbContext _context;
+
+        public ProjectRepository(TestManagementDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Project?> GetProjectWithSuitesAsync(int projectId)
+        {
+            return await _context.Projects
+                .Include(_ => _.TestSuites)
+                .ThenInclude(_ => _.TestCases)
+                .FirstOrDefaultAsync(_ => _.Id == projectId);
+        }
+    }
+}
