a8084be2642546c9df98f809649992745c050fc6
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-09 20:44:09 +0900

    Add ProjectService class. Add service class for project to wrap its repository.

---
+++ dev/src/TestManagement/TestManagement/Serivces/ProjectService.cs
@@ -0,0 +1,31 @@
+﻿using TestManagement.Data.Repositories;
+using TestManagement.Model;
+
+namespace TestManagement.Serivces
+{
+    public class ProjectService
+    {
+        private readonly IProjectRepository _projectRepository;
+
+        public ProjectService(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<IEnumerable<Project>> GetAllAsync()
+        {
+            return await _projectRepository.GetAllAsync();
+        }
+
+        public async Task<Project?> GetProjectWithSuiteAsync(int projectId)
+        {
+            return await _projectRepository.GetProjectWithSuitesAsync(projectId);
+        }
+
+        public async Task AddAsync(Project project)
+        {
+            await _projectRepository.AddAsync(project);
+            await _projectRepository.SaveChangesAsync();
+        }
+    }
+}
