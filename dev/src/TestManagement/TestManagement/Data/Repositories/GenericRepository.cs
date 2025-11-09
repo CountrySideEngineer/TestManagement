77df08e94607ba8ae77ac3e9a148349d1047cf0b
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-09 17:24:40 +0900

    Add GenericRepository class.

---
+++ dev/src/TestManagement/TestManagement/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+namespace TestManagement.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly TestManagementDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(TestManagementDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+        public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate) => await _dbSet.Where(predicate).ToListAsync();
+
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+        public void Update(T entity) => _dbSet.Update(entity);
+
+        public void Remove(T entity) => _dbSet.Remove(entity);
+
+        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+    }
+}
