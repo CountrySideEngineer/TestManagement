2ac77f4c542623f1e9e51bed6ac2a5c72ff33494
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-11 22:14:54 +0900

    Add classes about TestSuite repository. Add interface for TestSuiteRepository, ITestSuiteRepository. Add concrete class for test suite repository, TestSuiteRepository.

---
+++ dev/src/TestManagement/TestManagement/Data/Repositories/ITestSuiteRepository.cs
@@ -0,0 +1,13 @@
+﻿using TestManagement.Model;
+
+namespace TestManagement.Data.Repositories
+{
+    public interface ITestSuiteRepository
+    {
+        Task<IEnumerable<TestSuite>> GetAllAsync();
+        Task<TestSuite?> GetByIdAsync(int id);
+        Task AddAsync(TestSuite suite);
+        Task UpdateAsync(TestSuite suite);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ dev/src/TestManagement/TestManagement/Data/Repositories/TestSuiteRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using TestManagement.Model;
+
+namespace TestManagement.Data.Repositories
+{
+    public class TestSuiteRepository
+    {
+        private readonly TestManagementDbContext _context;
+
+        public TestSuiteRepository(TestManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TestSuite>> GetAllAsync()
+        {
+            return await _context.TestSuites
+                .Include(ts => ts.Project)
+                .ToListAsync();
+        }
+
+        public async Task<TestSuite?> GetByIdAsync(int id)
+        {
+            return await _context.TestSuites
+                .Include(ts => ts.Project)
+                .FirstOrDefaultAsync(_ => _.Id == id);
+        }
+
+        public async Task AddAsync(TestSuite suite)
+        {
+            _context.TestSuites.Add(suite);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TestSuite suite)
+        {
+            _context.Entry(suite).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var suite = await _context.TestSuites.FindAsync(id);
+            if (null != suite)
+            {
+                _context.TestSuites.Remove(suite);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
