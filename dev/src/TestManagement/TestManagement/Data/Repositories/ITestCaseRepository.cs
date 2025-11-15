565e5533cd554c0755fff864ae9493004ff03069
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-15 10:20:30 +0900

    Add TestCaseRepository classes. Add interface and concrete class for TestCaseRepository.

---
+++ dev/src/TestManagement/TestManagement/Data/Repositories/ITestCaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using TestManagement.Model;
+
+namespace TestManagement.Data.Repositories
+{
+    public interface ITestCaseRepository
+    {
+        Task<IEnumerable<TestCase>> GetAllAsync();
+        Task<TestCase?> GetByIdAsync(int id);
+        Task AddAsync(TestCase testCase);
+        Task UpdateAsync(TestCase testCase);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ dev/src/TestManagement/TestManagement/Data/Repositories/TestCaseRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestManagement.Model;
+
+namespace TestManagement.Data.Repositories
+{
+    public class TestCaseRepository : ITestCaseRepository
+    {
+        private readonly TestManagementDbContext _context;
+
+        public TestCaseRepository(TestManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TestCase>> GetAllAsync()
+        {
+            return await _context.TestCases
+                .Include(_ => _.TestResults)
+                .ToListAsync();
+        }
+
+        public async Task<TestCase?> GetByIdAsync(int id)
+        {
+            return await _context.TestCases
+                .Include(_ => _.TestResults)
+                .Where(_ => _.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task AddAsync(TestCase testCase)
+        {
+            _context.TestCases.Add(testCase);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TestCase testCase)
+        {
+            _context.Entry(testCase).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var testCase = await _context.TestCases.FindAsync(id);
+            if (null != testCase)
+            {
+                _context.TestCases.Remove(testCase);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
