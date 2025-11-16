b58f7f87eee439a35aae64b8bb268cf7727d722b
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-16 15:52:52 +0900

    Add TestCase model classes.

---
+++ dev/src/TestManagement.API/TestManagement.API/Models/TestCase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestManagement.API.Models
+{
+    public class TestCase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(2000)]
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        // Foregin key to TestLevel
+        public int TestLevelId { get; set; }
+        public TestLevel TestLevel { get; set; } = new TestLevel();
+    }
+}
