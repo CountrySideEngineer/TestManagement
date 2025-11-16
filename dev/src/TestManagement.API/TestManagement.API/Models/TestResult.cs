895b1a721a9b8145f348254abbabd9a64b6c7ce2
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-16 15:58:23 +0900

    Add TestResult model class.

---
+++ dev/src/TestManagement.API/TestManagement.API/Models/TestResult.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestManagement.API.Models
+{
+    public class TestResult
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ActualResult { get; set; } = string.Empty;
+
+        public int TestCaseId { get; set; }
+        public TestCase TestCase { get; set; } = new TestCase();
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
