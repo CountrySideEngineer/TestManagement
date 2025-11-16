ca90354ee56db2c46f42beb9e4166ab49fead629
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-16 17:27:31 +0900

    Add TestManagementDbContext class. Add DbContext class named "TestManagementDbContext". !!!Attention!!! Start implementing the class. The access to data base is not set and tested. Just starting implementing.

---
+++ dev/src/TestManagement.API/TestManagement.API/Data/TestManagementDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using TestManagement.API.Models;
+
+namespace TestManagement.API.Data
+{
+    public class TestManagementDbContext : DbContext
+    {
+        public TestManagementDbContext(DbContextOptions<TestManagementDbContext> options) : base(options) { }
+
+        public DbSet<TestCase> TestCases { get; set; }
+        public DbSet<TestLevel> TestLevels { get; set; }
+        public DbSet<TestResult> TestResults { get; set; }
+        public DbSet<TestRun> TestRuns { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Additional model configuration can go here
+        }
+    }
+}
