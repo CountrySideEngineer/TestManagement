efbcd5752a11b1321dfeb8ca0b938cdcbeafc26b
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-22 21:57:36 +0900

    Add repository interface and class for TestCase model.

---
+++ dev/src/TestManagement.API/TestManagement.API/Data/Repositories/ITestCaseRepository.cs
@@ -0,0 +1,11 @@
+﻿using TestManagement.API.Models;
+
+namespace TestManagement.API.Data.Repositories
+{
+    public interface ITestCaseRepository
+    {
+        Task<ICollection<TestCase>> GetAllAsync();
+
+        Task<ICollection<TestCase>> GetByIdAsync(int testLevelId);
+    }
+}
---
+++ dev/src/TestManagement.API/TestManagement.API/Data/Repositories/TestCaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestManagement.API.Models;
+
+namespace TestManagement.API.Data.Repositories
+{
+    public class TestCaseRepository : ITestCaseRepository
+    {
+        private readonly TestManagementDbContext _context;
+
+        public TestCaseRepository(TestManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<TestCase>> GetAllAsync()
+        {
+            return await _context.TestCases.ToListAsync();
+        }
+
+        public async Task<ICollection<TestCase>> GetByIdAsync(int testLevelId)
+        {
+            return await _context.TestCases
+                .Where(_ => _.TestLevelId == testLevelId)
+                .ToListAsync();
+        }
+    }
+}
