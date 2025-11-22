fd3eb12c426c05a0707c45ee12e378ce608dd954
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-22 21:57:54 +0900

    Add TestCaseService class.

---
+++ dev/src/TestManagement.API/TestManagement.API/Services/TestCaseService.cs
@@ -0,0 +1,25 @@
+﻿using TestManagement.API.Data.Repositories;
+using TestManagement.API.Models;
+
+namespace TestManagement.API.Services
+{
+    public class TestCaseService
+    {
+        private readonly ITestCaseRepository _testCaseRepository;
+
+        public TestCaseService(ITestCaseRepository testCaseRepository)
+        {
+            _testCaseRepository = testCaseRepository;
+        }
+
+        public async Task<ICollection<TestCase>> GetAllAsync()
+        {
+            return await _testCaseRepository.GetAllAsync();
+        }
+
+        public async Task<ICollection<TestCase>> GetByIdAsync(int testLevelId)
+        {
+            return await _testCaseRepository.GetByIdAsync(testLevelId);
+        }
+    }
+}
