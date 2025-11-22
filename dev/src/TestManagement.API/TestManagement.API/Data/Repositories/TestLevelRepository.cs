cf34817001e660d5c318e87e0bfd0db88a3d472a
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-22 19:59:44 +0900

    Add TestLevelRepository class and its interface.

---
+++ dev/src/TestManagement.API/TestManagement.API/Data/Repositories/TestLevelRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestManagement.API.Models;
+
+namespace TestManagement.API.Data.Repositories
+{
+    public class TestLevelRepository : ITestLevelRepository
+    {
+        private readonly TestManagementDbContext _context;
+
+        public TestLevelRepository(TestManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<TestLevel>> GetAllAsync()
+        {
+            return await _context.TestLevels.ToListAsync();
+        }
+    }
+}
