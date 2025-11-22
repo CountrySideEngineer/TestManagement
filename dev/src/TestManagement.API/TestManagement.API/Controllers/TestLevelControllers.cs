7de64372af3c2c4db561f8fcfd0d0ccfba791597
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-22 20:17:23 +0900

    Add TestLevelControllers class.

---
+++ dev/src/TestManagement.API/TestManagement.API/Controllers/TestLevelControllers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestManagement.API.Services;
+
+namespace TestManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestLevelControllers : ControllerBase
+    {
+        private readonly TestLevelService _testLevelService;
+
+        public TestLevelControllers(TestLevelService testLevelRepository)
+        {
+            _testLevelService = testLevelRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTestLevels()
+        {
+            var testLevels = await _testLevelService.GetAllAsync();
+            return Ok(testLevels);
+        }
+    }
+}
