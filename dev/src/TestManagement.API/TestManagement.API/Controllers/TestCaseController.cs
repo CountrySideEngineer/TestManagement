651fc1cea9c540c3fd4773fb533da5f0ba07370d
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-22 21:58:19 +0900

    Add TestCaseController class.

---
+++ dev/src/TestManagement.API/TestManagement.API/Controllers/TestCaseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestManagement.API.Services;
+
+namespace TestManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestCaseController : ControllerBase
+    {
+        private readonly TestCaseService _testCaseService;
+
+        public TestCaseController(TestCaseService testCaseService)
+        {
+            _testCaseService = testCaseService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTestCases()
+        {
+            var testCases = await _testCaseService.GetAllAsync();
+            return Ok(testCases);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTestCasesByTestLevelId(int id)
+        {
+            var testCases = await _testCaseService.GetByIdAsync(id);
+            return Ok(testCases);
+        }
+    }
+}
