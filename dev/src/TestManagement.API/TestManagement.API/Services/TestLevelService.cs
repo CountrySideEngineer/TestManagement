948cfd365b0839345bab8b2f22384e6cbad2a448
CountrySideEngineer <deep-sleeper@nifty.com>
2025-11-22 20:16:26 +0900

    Add service class for TestLevel table.

---
+++ dev/src/TestManagement.API/TestManagement.API/Services/TestLevelService.cs
@@ -0,0 +1,20 @@
+﻿using TestManagement.API.Data.Repositories;
+using TestManagement.API.Models;
+
+namespace TestManagement.API.Services
+{
+    public class TestLevelService
+    {
+        private readonly ITestLevelRepository _testLevelRepository;
+
+        public TestLevelService(ITestLevelRepository testLevelRepository)
+        {
+            _testLevelRepository = testLevelRepository;
+        }
+
+        public async Task<ICollection<TestLevel>> GetAllAsync()
+        {
+            return await _testLevelRepository.GetAllAsync();
+        }
+    }
+}
