c6b55d265ab3cfe1f19f6823a01a093867e28ac2
CountrySideEngineer <deep-sleeper@nifty.com>
2025-12-03 22:31:55 +0900

    Add Dto classes.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Models/TestResultDto.cs
@@ -0,0 +1,22 @@
+﻿namespace TestManagement.APP.Models
+{
+    public enum TestStatus
+    {
+        Unknown = 0,
+        Success = 1,
+        Failure = 2,
+        Skipped = 3,
+        Blocked = 4
+    }
+
+    public class TestResultDto
+    {
+        public int Id { get; set; }
+        
+        public string ActualResult { get; set; } = string.Empty;
+
+        public int TestRunId { get; set; }
+
+        public TestStatus Status { get; set; } = TestStatus.Unknown;
+    }
+}
