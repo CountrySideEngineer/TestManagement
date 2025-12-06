bdf045f958c1c4e09ca9bbf33bf70c2c8cd65943
CountrySideEngineer <deep-sleeper@nifty.com>
2025-12-07 06:05:28 +0900

    Add new model class. Add model classes to handle test result analysis request.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Models/TestAnalysis/ResultMaster.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace TestManagement.APP.Models.TestAnalysis
+{
+    public class ResultMaster
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation property to related Request objects.
+        [JsonIgnore]
+        public ICollection<Request> Requests { get; set; } = new List<Request>();
+    }
+}
