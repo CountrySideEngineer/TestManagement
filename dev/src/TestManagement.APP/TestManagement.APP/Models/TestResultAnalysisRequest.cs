c1c7debbbccc65bb86c05a64c983b21ce7460aeb
CountrySideEngineer <deep-sleeper@nifty.com>
2025-12-06 23:02:14 +0900

    Add TestResultAnalysisRequest class.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Models/TestResultAnalysisRequest.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestManagement.APP.Models
+{
+    public enum Status
+    {
+        NotStarted,
+        Running,
+        Finished
+    };
+
+    public enum Result
+    {
+        Unknown,
+        Passed,
+        Failed
+    };
+
+    public class TestResultAnalysisRequest
+    {
+        [Key]
+        public int Key { get; set; }
+
+        [Required]
+        public string DirectoryPath { get; set; } = string.Empty;
+
+        public Status Status { get;set; } = Status.NotStarted;
+
+        public Result Result { get;set; } = Result.Unknown;
+
+        public DateTime RegisteredAt { get;set; } = DateTime.UtcNow;
+
+        public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+    }
+}
