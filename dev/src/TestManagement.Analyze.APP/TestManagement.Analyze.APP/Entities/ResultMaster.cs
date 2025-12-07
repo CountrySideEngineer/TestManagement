a6103510f52147faa02184034d1f4d858b31f0b1
CountrySideEngineer <deep-sleeper@nifty.com>
2025-12-07 16:41:55 +0900

    Add DbContext class and entities. DbContext and entity classes generated by scaffolding. When scaffolding, the command below executed from "Package manager console" in VisualStudio.
    dotnet ef dbcontext scaffold "Host=xxx.xxx.xxx.xxx:yyyy;Database=Aaaa;Username=Bbbb;Password="YOUR_PASSWORD" Npgsql.EntityFrameworkCore.PostgreSQL --data-annotations --context-dir DBContext/  --output-dir Entities/ --force -v --project TestManagement.Analyze.APP
    
    When execute, replace "xxx.xxx.xxx.xxx" by actual database server address, and "YYYY" by port number, "Aaaa" database name, "Bbbb" user name, "YOUR_PASSWORD" your password.
    This application access PostgreSQL data base.

---
+++ dev/src/TestManagement.Analyze.APP/TestManagement.Analyze.APP/Entities/ResultMaster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestManagement.Analyze.APP.Entities;
+
+public partial class ResultMaster
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    [InverseProperty("Result")]
+    public virtual ICollection<Request> Requests { get; set; } = new List<Request>();
+}
---
+++ dev/src/TestManagement.Analyze.APP/TestManagement.Analyze.APP/Entities/StatusMaster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestManagement.Analyze.APP.Entities;
+
+public partial class StatusMaster
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    [InverseProperty("Status")]
+    public virtual ICollection<Request> Requests { get; set; } = new List<Request>();
+}
