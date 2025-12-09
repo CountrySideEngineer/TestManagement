6126d271964bf993ead93233e663adf8b3ca0fe9
CountrySideEngineer <deep-sleeper@nifty.com>
2025-12-09 23:02:11 +0900

    Add DTO model classes.

---
+++ dev/src/TestManagement.Analyze.APP/TestManagement.Analyze.APP/Model/DTO/TestLevelDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestManagement.Analyze.APP.Model.DTO
+{
+    internal class TestLevelDto
+    {
+        public class TestLevelModel
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; } = string.Empty;
+
+            public string Description { get; set; } = string.Empty;
+
+            public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+            public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        }
+    }
+}
---
+++ dev/src/TestManagement.Analyze.APP/TestManagement.Analyze.APP/Model/DTO/TestResultDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestManagement.Analyze.APP.Model.DTO
+{
+    internal class TestResultDto
+    {
+        public int Id { get; set; }
+
+        public string ActualResult { get; set; } = string.Empty;
+
+        public int TestCaseId { get; set; }
+        public TestCaseDto TestCase { get; set; } = new ();
+
+        public int TestRunId { get; set; }
+        public TestRunDto TestRun { get; set; } = new ();
+
+
+        public DateTime ExecutedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
