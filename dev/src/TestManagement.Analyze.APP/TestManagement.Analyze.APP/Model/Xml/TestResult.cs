b1b7820ed90f3b899837884b6c4aa4abe19032dd
CountrySideEngineer <deep-sleeper@nifty.com>
2025-12-09 23:01:44 +0900

    Add TestResult.cs and TestResultReader class. Add TestResult.cs which contains classes about test result xml file object. And TestResultReader, a reader class, to read xml file and convert them to class objects defined in TestResult.cs file.

---
+++ dev/src/TestManagement.Analyze.APP/TestManagement.Analyze.APP/Model/Xml/TestResult.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TestManagement.Analyze.APP.Model.Xml
+{
+    [XmlRoot("testsuites")]
+    public class TestSuites
+    {
+        [XmlAttribute("tests")]
+        public int Tests { get; set; }
+
+        [XmlAttribute("failures")]
+        public int Failures { get; set; }
+
+        [XmlAttribute("disabled")]
+        public int Disabled { get; set; }
+
+        [XmlAttribute("errors")]
+        public int Errors { get; set; }
+
+        [XmlAttribute("time")]
+        public float Time { get; set; }
+
+        [XmlAttribute("timestamp")]
+        public DateTime TimeStamp { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [XmlElement("testsuite")]
+        public List<TestSuite> TestItems { get; set; } = new();
+    }
+
+    [XmlRoot("testsuite")]
+    public class TestSuite
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [XmlAttribute("tests")]
+        public int Tests { get; set; }
+
+        [XmlAttribute("failures")]
+        public int Failures { get; set; }
+
+        [XmlAttribute("disabled")]
+        public int Disabled { get; set; }
+
+        [XmlAttribute("errors")]
+        public int Errors { get; set; }
+
+        [XmlAttribute("time")]
+        public float Time { get; set; }
+
+        [XmlAttribute("timestamp")]
+        public DateTime TimeStamp { get; set; }
+
+        [XmlElement("testcase")]
+        public List<TestCase> TestCases { get; set; } = new();
+    }
+
+    [XmlRoot("testcase")]
+    public class TestCase
+    {
+        public TestCase()
+        {
+            Failure = null;
+        }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [XmlAttribute("status")]
+        public string Status { get; set; } = string.Empty;
+
+        [XmlAttribute("result")]
+        public string Result { get; set; } = string.Empty;
+
+        [XmlAttribute("time")]
+        public float Time { get; set; }
+
+        [XmlAttribute("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [XmlAttribute("classname")]
+        public string ClassName { get; set; } = string.Empty;
+
+        [XmlElement("failure")]
+        public Failure? Failure { get; set; }
+
+        public string Judge
+        {
+            get
+            {
+                if (null == Failure)
+                {
+                    return "OK";
+                }
+                else
+                {
+                    return "NG";
+                }
+
+            }
+        }
+
+        public bool IsFail
+        {
+            get
+            {
+                if (null == Failure)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+    }
+
+    [XmlRoot("failure")]
+    public class Failure
+    {
+        [XmlAttribute("message")]
+        public string Message { get; set; } = string.Empty;
+    }
+}
---
+++ dev/src/TestManagement.Analyze.APP/TestManagement.Analyze.APP/Model/Xml/TestResultReader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TestManagement.Analyze.APP.Model.Xml
+{
+    internal class TestResultReader
+    {
+        public TestResultReader() { }
+
+        public virtual IEnumerable<TestSuites> ReadFromDir(string dirPath)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(dirPath);
+            var filesInDir = dirInfo.GetFiles("*.xml", SearchOption.TopDirectoryOnly);
+
+            var testSuitesCollection = new List<TestSuites>();
+            foreach (var fileItem in filesInDir)
+            {
+                string filePath = fileItem.FullName;
+                var testSuitesItem = Read(filePath);
+                testSuitesCollection.Add(testSuitesItem);
+            }
+
+            return testSuitesCollection;
+        }
+
+        public virtual TestSuites Read(string filePath)
+        {
+            try
+            {
+                using var reader = new StreamReader(filePath, false);
+                var serialize = new XmlSerializer(typeof(TestSuites));
+                TestSuites testSuites = (TestSuites)serialize.Deserialize(reader)!;
+
+                return testSuites;
+            }
+            catch (Exception ex)
+            when ((ex is ArgumentException) || (ex is ArgumentNullException))
+            {
+                throw new ArgumentException();
+            }
+            catch (Exception ex)
+            when ((ex is FileNotFoundException) || (ex is DirectoryNotFoundException))
+            {
+                throw new FileNotFoundException();
+            }
+            catch (InvalidOperationException)
+            {
+                throw;
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+
+
+        }
+    }
+}
