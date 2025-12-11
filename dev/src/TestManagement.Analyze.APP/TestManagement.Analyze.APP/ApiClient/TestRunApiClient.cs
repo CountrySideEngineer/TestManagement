519db8b3d8138c612bc86136b11d17caeee460b8
CountrySideEngineer <deep-sleeper@nifty.com>
2025-12-12 06:21:01 +0900

    Add TestRunApiClient class.

---
+++ dev/src/TestManagement.Analyze.APP/TestManagement.Analyze.APP/ApiClient/TestRunApiClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using TestManagement.Analyze.APP.Model.DTO;
+
+namespace TestManagement.Analyze.APP.ApiClient
+{
+    internal class TestRunApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public TestRunApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public TestRunDto? Add(TestRunDto testRun)
+        {
+            Task<HttpResponseMessage> responseTask = _httpClient.PostAsJsonAsync("TestRun", testRun);
+            responseTask.Wait();
+            HttpResponseMessage response = responseTask.Result;
+            if (response.IsSuccessStatusCode)
+            {
+                Task<TestRunDto?> testRunTask =
+                    response.Content.ReadFromJsonAsync<TestRunDto>();
+                testRunTask.Wait();
+                TestRunDto? newTestRun = testRunTask.Result;
+                return newTestRun;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
