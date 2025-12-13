ba09e698ddb0d2ff6210f6fb21e5630cab17b677
CountrySideEngineer <deep-sleeper@nifty.com>
2025-12-14 07:28:22 +0900

    Update TestSuitesConverter class. Add methods to convert to TestResultDto objects.

--- dev/src/TestManagement.Analyze.APP/TestManagement.Analyze.APP/Model/Converter/TestSuitesConverter.cs
+++ dev/src/TestManagement.Analyze.APP/TestManagement.Analyze.APP/Model/Converter/TestSuitesConverter.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.XPath;
 using TestManagement.Analyze.APP.Model.DTO;
 using TestManagement.Analyze.APP.Model.Xml;
 
@@ -56,30 +57,82 @@ namespace TestManagement.Analyze.APP.Model.Converter
             var builder = new TestCaseBuilder(testSuite.Name);
             foreach (var testCase in testSuite.TestCases)
             {
-                string testCaseName = builder.MakeTestCaseName(testCase.Name);
-                var testCaseDto = new TestCaseDto()
-                {
-                    Title = testCaseName,
-                    Description = testCase.Name,
-                    TestLevelId = 1,
-                    TestLevel = new TestLevelDto()
-                    {
-                        Id = 1,
-                        Name = "",
-                        Description = "",
-                    },
-                };
-                yield return testCaseDto;
+                yield return ToTestCase(testCase, builder);
             }
         }
 
-        public IEnumerable<TestResultDto> ToTestResult(IEnumerable<TestSuites> testSuitesCollection)
+        public TestCaseDto ToTestCase(TestCase testCase, TestCaseBuilder builder)
         {
+            string testCaseName = builder.MakeTestCaseName(testCase.Name);
+            var testCaseDto = new TestCaseDto()
+            {
+                Title = testCaseName,
+                Description = testCase.Name,
+                TestLevelId = 1,
+                TestLevel = new TestLevelDto()
+                {
+                    Id = 1,
+                    Name = "Name",
+                    Description = "Description",
+                },
+            };
+            return testCaseDto;
+        }
 
+        public IEnumerable<TestResultDto> ToTestResults(IEnumerable<TestSuites> testSuitesCollection)
+        {
+            var testResultsCollection = new List<TestResultDto>();
+            foreach (var testSuitesItem in testSuitesCollection)
+            {
+                IEnumerable<TestResultDto> testResults = ToTestResults(testSuitesItem);
+                testResultsCollection.AddRange(testResults);
+            }
 
+            return testResultsCollection;
+        }
 
+        public IEnumerable<TestResultDto> ToTestResults(TestSuites testsSuites)
+        {
+            return ToTestResults(testsSuites.TestItems);
+        }
 
-            return null;
+        public IEnumerable<TestResultDto> ToTestResults(IEnumerable<TestSuite> testSuiteCollection)
+        {
+            var testResultsCollection = new List<TestResultDto>();
+            foreach (var testSuite in testSuiteCollection)
+            {
+                var testResults = ToTestResults(testSuite);
+                testResultsCollection.AddRange(testResults);
+            }
+
+            return testResultsCollection;
+        }
+
+        public IEnumerable<TestResultDto> ToTestResults(TestSuite testSuite)
+        {
+            var builder = new TestCaseBuilder(testSuite.Name);
+            foreach (var testCase in testSuite.TestCases)
+            {
+                yield return ToTestResult(testCase, builder);
+            }
+        }
+
+        public TestResultDto ToTestResult(TestCase testCase, TestCaseBuilder builder)
+        {
+            TestResultDto resultDto = new();
+            if (testCase.ResultCode == TestResultCode.Success)
+            {
+                resultDto.Status = TestStatus.Success;
+                resultDto.ActualResult = "SUCCESS";
+            }
+            else
+            {
+                resultDto.Status = TestStatus.Failure;
+                resultDto.ActualResult = "FAIL";
+            }
+            TestCaseDto testCaseDto = ToTestCase(testCase, builder);
+            resultDto.TestCase = testCaseDto;
+            return resultDto;
         }
 
     }
