a4d635768bfea50da525fad2072224082447686d
CountrySideEngineer <deep-sleeper@nifty.com>
2025-12-26 17:14:06 +0900

    Add uncommitted file.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Models/TestCaseDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace TestManagement.APP.Models
+{
+    public class TestCaseDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        // Foregin key to TestLevel
+        public int TestLevelId { get; set; }
+        public TestLevelDto? TestLevel { get;set; }
+    }
+}
