d0b3d9ae086462311b05341717306a156e32d78e
CountrySideEngineer <deep-sleeper@nifty.com>
2026-01-01 07:35:39 +0900

    Update RequestDbContext. Add default records to "StatusMasters" and "ResultMasters" table in request data base.

--- dev/src/TestManagement.Analyze.APP/TestManagement.Analyze.APP/DBContext/RequestDbContext.cs
+++ dev/src/TestManagement.Analyze.APP/TestManagement.Analyze.APP/DBContext/RequestDbContext.cs
@@ -28,6 +28,26 @@ public partial class RequestDbContext : DbContext
             entity.HasOne(d => d.Status).WithMany(p => p.Requests).OnDelete(DeleteBehavior.Restrict);
         });
 
+        var seedDate = new DateTime(2025, 11, 21, 0, 0, 0, DateTimeKind.Utc);
+        modelBuilder.Entity<StatusMaster>(_ =>
+        {
+            _.HasData(
+                new StatusMaster { Id = 1, Name = "Not started", CreatedAt = seedDate, UpdatedAt = seedDate },
+                new StatusMaster { Id = 2, Name = "Running", CreatedAt = seedDate, UpdatedAt = seedDate },
+                new StatusMaster { Id = 3, Name = "Completed(success)", CreatedAt = seedDate, UpdatedAt = seedDate },
+                new StatusMaster { Id = 4, Name = "Completed(failed)", CreatedAt = seedDate, UpdatedAt = seedDate }
+                );
+        });
+
+        modelBuilder.Entity<ResultMaster>(_ =>
+        {
+            _.HasData(
+                new ResultMaster { Id = 1, Name = "Unknown", CreatedAt = seedDate, UpdatedAt = seedDate },
+                new ResultMaster { Id = 2, Name = "Passed", CreatedAt = seedDate, UpdatedAt = seedDate },
+                new ResultMaster { Id = 3, Name = "Failed", CreatedAt = seedDate, UpdatedAt = seedDate }
+                );
+        });
+
         OnModelCreatingPartial(modelBuilder);
     }
 
