5b65838b8119cfbe92c0557530ca3e9f4ccc72fe
CountrySideEngineer <deep-sleeper@nifty.com>
2026-01-06 22:45:36 +0900

    Add TestRunApiClient class. Add API client class to get test run informations.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Services/TestRunApiClient.cs
@@ -0,0 +1,21 @@
+﻿using TestManagement.APP.Models;
+
+namespace TestManagement.APP.Services
+{
+    public class TestRunApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public TestRunApiClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("TestApiClient");
+        }
+
+        public async Task<List<TestRunDto>> GetTestRunsAsync()
+        {
+            var response = await _httpClient.GetFromJsonAsync<List<TestRunDto>>("api/TestRun");
+
+            return response;
+        }
+    }
+}
