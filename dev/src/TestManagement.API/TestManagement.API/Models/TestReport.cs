799b69a5a5a627d8d5c17b643c0a1fa31f57da4b
CountrySideEngineer <deep-sleeper@nifty.com>
2026-02-01 21:55:27 +0900

    Add TestReport class. Added the TestReport class as a model class to store test result reports.

---
+++ dev/src/TestManagement.API/TestManagement.API/Models/TestReport.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestManagement.API.Models
+{
+    public class TestReport
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int TestRunId { get; set; } = 0;
+        public TestRun TestRun { get; set; } = new();
+
+        public string Path { get; set; } = string.Empty;
+
+        public string Report { get; set; } = string.Empty;
+
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime Updateat { get; set; } = DateTime.UtcNow;
+    }
+}
