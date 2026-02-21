fd67c78e348b8e84a2296583ce511f71bf2994a1
CountrySideEngineer <deep-sleeper@nifty.com>
2026-02-22 07:27:10 +0900

    Add "ParseOption" class. Add a new class, "ParseOption", to store optional information such as the test level ID and the test execution information ID.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Services/Option/ParseOption.cs
@@ -0,0 +1,18 @@
+﻿namespace TestManagement.APP.Services.Option
+{
+    /// <summary>
+    /// Options used when parsing a file.
+    /// </summary>
+    public class ParseOption
+    {
+        /// <summary>
+        /// Identifier of the test level. Optional (when null the default level is used).
+        /// </summary>
+        public int? TestLevelId { get; set; } 
+
+        /// <summary>
+        /// Revision identifier. Specifies the version of the upload or the item being parsed.
+        /// </summary>
+        public int RevisionId { get; set; }
+    }
+}
