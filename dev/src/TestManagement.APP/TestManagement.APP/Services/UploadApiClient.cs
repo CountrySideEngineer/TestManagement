af97884b78c7e3f57d2a20ed1b21082e4ad5a6e7
CountrySideEngineer <deep-sleeper@nifty.com>
2026-02-24 21:49:37 +0900

    Update "UploadApiClient" class. Add method to get and create "TestRun".

--- dev/src/TestManagement.APP/TestManagement.APP/Services/UploadApiClient.cs
+++ dev/src/TestManagement.APP/TestManagement.APP/Services/UploadApiClient.cs
@@ -23,5 +23,41 @@ namespace TestManagement.APP.Services
                 return testLevels;
             }
         }
+
+        public async Task<List<TestRunDto>> GetLatestTestRunAsync()
+        {
+            var testRuns = await _httpClient.GetFromJsonAsync<List<TestRunDto>>("api/testrun/");
+            if (null == testRuns)
+            {
+                return new List<TestRunDto>();
+            }
+
+            List<TestResultDto>? testResults = await _httpClient.GetFromJsonAsync<List<TestResultDto>>("api/testresult");
+            if (null == testResults)
+            {
+                return testRuns;
+            }
+
+            foreach (var testRun in testRuns)
+            {
+                testRun.TestResults = testResults.Where(_ => _.TestRunId == testRun.Id).ToList();
+            }
+
+            return testRuns;
+        }
+
+        public async Task<TestRunDto?> CreateTestRunAsync(TestRunDto newRun)
+        {
+            if (newRun == null) throw new ArgumentNullException(nameof(newRun));
+
+            var response = await _httpClient.PostAsJsonAsync("api/TestRun", newRun);
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var created = await response.Content.ReadFromJsonAsync<TestRunDto>();
+            return created;
+        }
     }
 }
