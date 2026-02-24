f8a4e38f22300826d0afaa6d0ce53fcbfa877f01
CountrySideEngineer <deep-sleeper@nifty.com>
2026-02-24 21:38:42 +0900

    Add "UploadApiClient" class. Starting implementing the API class for the "Upload" page.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Services/UploadApiClient.cs
@@ -0,0 +1,27 @@
+﻿using TestManagement.APP.Models;
+
+namespace TestManagement.APP.Services
+{
+    public class UploadApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public UploadApiClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("TestApiClient");
+        }
+
+        public async Task<List<TestLevelDto>> GetTestLevelAsync()
+        {
+            var testLevels = await _httpClient.GetFromJsonAsync<List<TestLevelDto>>("api/testlevels");
+            if (null == testLevels)
+            {
+                return new List<TestLevelDto>();
+            }
+            else
+            {
+                return testLevels;
+            }
+        }
+    }
+}
