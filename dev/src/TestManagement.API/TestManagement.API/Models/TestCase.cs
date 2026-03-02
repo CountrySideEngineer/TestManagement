7d4df24cf5314171ebc1870b598631a40c81be5a
CountrySideEngineer <deep-sleeper@nifty.com>
2026-03-02 21:58:25 +0900

    Add new "TestCase" class.

---
+++ dev/src/TestManagement.API/TestManagement.API/Models/TestCase.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestManagement.API.Models
+{
+    public class TestCase
+    {
+        private readonly List<TestCaseVersion> _versions = new();
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get;set; } = DateTime.UtcNow;
+
+        public IReadOnlyCollection<TestCaseVersion> Versions => _versions;
+
+    }
+}
