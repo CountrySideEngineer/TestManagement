d811e424d864be82a21cd54fae39cf5c8ad530e7
CountrySideEngineer <deep-sleeper@nifty.com>
2026-03-02 23:08:29 +0900

    Update DbContext class of the API.

--- dev/src/TestManagement.API/TestManagement.API/Data/TestManagementDbContext.cs
+++ dev/src/TestManagement.API/TestManagement.API/Data/TestManagementDbContext.cs
@@ -8,36 +8,31 @@ namespace TestManagement.API.Data
     {
         public TestManagementDbContext(DbContextOptions<TestManagementDbContext> options) : base(options) { }
 
-        public DbSet<TestCaseVersion> TestCases { get; set; }
+        public DbSet<TestCaseVersion> TestCaseVersions { get; set; }
         public DbSet<TestLevel> TestLevels { get; set; }
         public DbSet<TestResult> TestResults { get; set; }
         public DbSet<TestRun> TestRuns { get; set; }
+        public DbSet<TestCase> TestCases { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            ConfigureTestLevel(modelBuilder);
+            ConfigureTestCase(modelBuilder);
+            ConfigureTestCaseVersion(modelBuilder);
+            ConfigureTestResult(modelBuilder);
+            ConfigureTestRun(modelBuilder);
+        }
+
+        private void ConfigureTestLevel(ModelBuilder modelBuilder)
+        {
             modelBuilder.Entity<TestLevel>()
                 .HasMany(_ => _.TestCases)
                 .WithOne(_ => _.TestLevel)
                 .HasForeignKey(_ => _.TestLevelId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<TestCaseVersion>()
-                .HasIndex(_ => new { _.Title, _.Description, _.TestLevelId })
-                .IsUnique();
-
-            modelBuilder.Entity<TestCaseVersion>()
-                .HasMany(_ => _.Results)
-                .WithOne(_ => _.TestCase)
-                .HasForeignKey(_ => _.TestCaseId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<TestRun>()
-                .HasMany(_ => _.TestResults)
-                .WithOne(_ => _.TestRun)
-                .HasForeignKey(_ => _.TestRunId)
-                .OnDelete(DeleteBehavior.Restrict);
 
             // Add seed TestLevel class (Unit, Integration, System, Acceptance)
             var seedDate = new DateTime(2025, 11, 21, 0, 0, 0, DateTimeKind.Utc);
@@ -49,6 +44,51 @@ namespace TestManagement.API.Data
                     new TestLevel { Id = 3, Name = "System", Description = "System Level Testing", CreatedAt = seedDate, UpdatedAt = seedDate },
                     new TestLevel { Id = 4, Name = "Acceptance", Description = "Acceptance Level Testing", CreatedAt = seedDate, UpdatedAt = seedDate });
             });
+        }
+
+        private void ConfigureTestResult(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TestResult>()
+                .HasOne(_ => _.TestCaseVersion)
+                .WithMany()
+                .HasForeignKey(_ => _.TestCaseVersionId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+        private void ConfigureTestRun(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TestRun>()
+                .HasMany(_ => _.TestResults)
+                .WithOne(_ => _.TestRun)
+                .HasForeignKey(_ => _.TestRunId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+        private void ConfigureTestCase(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TestCase>()
+                .HasMany(_ => _.Versions)
+                .WithOne(_ => _.TestCase)
+                .HasForeignKey(_ => _.TestCaseId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+        private void ConfigureTestCaseVersion(ModelBuilder builder)
+        {
+            var entity = builder.Entity<TestCaseVersion>();
+
+            entity.HasIndex(_ => new { _.TestCaseId, _.VersionNumber })
+                .IsUnique();
+
+            entity.HasIndex(_ => _.TestCaseId)
+                .IsUnique();
+
+            entity.HasIndex(_ => new { _.Title, _.Description, _.TestLevelId })
+                .IsUnique();
+
+            builder.Entity<TestCaseVersion>()
+                    .HasIndex(_ => new { _.Title, _.Description, _.TestLevelId })
+                    .IsUnique();
         }
     }
 }
