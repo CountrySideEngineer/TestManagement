17d1d722cc43b429c5c9d0eb40c4013b1aaa48ba
CountrySideEngineer <deep-sleeper@nifty.com>
2026-03-04 22:52:43 +0900

    Add "Environment" class. Add the class which holds an environment a test has been executed.

---
+++ dev/src/TestManagement.API/TestManagement.API/Models/Environment.cs
@@ -0,0 +1,17 @@
+﻿namespace TestManagement.API.Models
+{
+    public class Environment
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Os { get; set; }
+
+        public string? RunTime { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
