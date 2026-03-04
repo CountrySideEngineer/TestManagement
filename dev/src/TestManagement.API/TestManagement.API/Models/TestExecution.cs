87e08735bc6c81ac29b75c759272112d8aac521e
CountrySideEngineer <deep-sleeper@nifty.com>
2026-03-04 22:56:47 +0900

    Add "TestExecution" class. Added the TestExecution class to store the revision information, environment, and test execution results.

---
+++ dev/src/TestManagement.API/TestManagement.API/Models/TestExecution.cs
@@ -0,0 +1,18 @@
+﻿namespace TestManagement.API.Models
+{
+    public class TestExecution
+    {
+        public long Id { get; set; }
+
+        public string Revision { get; set; } = null!;
+
+        public long EnvironmentId { get; set; }
+
+        public DateTime ExecutedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Environment Environment { get; set; } = null!;
+        public ICollection<TestResult> TestResults { get; set; } = new List<TestResult>();
+    }
+}
