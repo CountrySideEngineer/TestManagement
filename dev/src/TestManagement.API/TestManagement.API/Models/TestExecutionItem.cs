40463f0d72a165b530c936c88818848b7c6f0e5d
CountrySideEngineer <deep-sleeper@nifty.com>
2026-03-13 20:57:47 +0900

    Add TestExecutionItem class.

---
+++ dev/src/TestManagement.API/TestManagement.API/Models/TestExecutionItem.cs
@@ -0,0 +1,17 @@
+﻿namespace TestManagement.API.Models
+{
+    public class TestExecutionItem
+    {
+        public int Id { get; set; }
+
+        public int TestExecutionId { get; set; } = 0;
+
+        public DateTime ExecutedAt { get; set; } = DateTime.UtcNow;
+
+        public int EnvironmentId { get; set; } = 0;
+
+        public DateTime CreatedAt { get;set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
