e30b81bbc8351fa2b1e41a0d99c10c3b526c80e6
CountrySideEngineer <deep-sleeper@nifty.com>
2026-03-20 07:13:35 +0900

    Add CreateTestCaseResponse class.

---
+++ dev/src/TestManagement.API/TestManagement.API/Features/TestCases/Create/CreateTestCaseResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace TestManagement.API.Features.TestCases.Create
+{
+    public class CreateTestCaseResponse
+    {
+        public int Id { get; set; } = 0;
+
+        public string Code { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public int TestLevelId { get; set; } = 0;
+
+        public int VersionNumber { get; set; } = 0;
+    }
+}
