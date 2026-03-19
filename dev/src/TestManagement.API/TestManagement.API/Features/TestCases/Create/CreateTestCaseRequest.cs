02bb71a7adc3bd055b559627e3b0032956eb82cf
CountrySideEngineer <deep-sleeper@nifty.com>
2026-03-20 06:37:34 +0900

    Implement CreateTestCaseRequest class. CreateTestCase DTO class.

--- dev/src/TestManagement.API/TestManagement.API/Features/TestCases/Create/CreateTestCaseRequest.cs
+++ dev/src/TestManagement.API/TestManagement.API/Features/TestCases/Create/CreateTestCaseRequest.cs
@@ -2,5 +2,12 @@
 {
     public class CreateTestCaseRequest
     {
+        public string Code { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public int TestLevelId { get; set; } = 0;
     }
 }
