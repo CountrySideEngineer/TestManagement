0978f66b1b51410dd93d703a14e74fd6f01cc4df
CountrySideEngineer <deep-sleeper@nifty.com>
2026-03-23 22:18:16 +0900

    Add "GetTestCaseResponse" class.

---
+++ dev/src/TestManagement.API/TestManagement.API/Features/TestCases/Get/GetTestCaseResponse.cs
@@ -0,0 +1,38 @@
+﻿namespace TestManagement.API.Features.TestCases.Get
+{
+    /// <summary>
+    /// Response DTO returned when retrieving test case information (latest version per case).
+    /// </summary>
+    public class GetTestCaseResponse
+    {
+        /// <summary>
+        /// Identifier of the test case version record.
+        /// </summary>
+        public long Id { get; set; } = 0;
+
+        /// <summary>
+        /// Business code of the test case.
+        /// </summary>
+        public string Code { get; set; } = null!;
+
+        /// <summary>
+        /// Name/title of the test case version.
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Description of the test case version.
+        /// </summary>
+        public string Description { get; set; } = null!;
+
+        /// <summary>
+        /// Identifier of the test level associated with this version.
+        /// </summary>
+        public long TestLevelId { get; set; } = 0;
+
+        /// <summary>
+        /// The version number for this test case version.
+        /// </summary>
+        public int VersionNumber { get; set; } = 0;
+    }
+}
