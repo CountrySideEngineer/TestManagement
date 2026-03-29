84cf1d3b9584dbfc51d00271a0eb5d140c95d341
CountrySideEngineer <deep-sleeper@nifty.com>
2026-03-29 17:55:18 +0900

    Add environment service and its dto classes.

---
+++ dev/src/TestManagement.API/TestManagement.API/Features/Environment/Get/GetEnvironmentRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TestManagement.API.Features.Environment.Get
+{
+    public class GetEnvironmentRequest
+    {
+        public string? Name { get; set; } = null!;
+
+        public string? Os { get; set; } = null!;
+
+        public string? RunTime { get; set; } = null!;
+    }
+}
---
+++ dev/src/TestManagement.API/TestManagement.API/Services/EnvironmentService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestManagement.API.Data;
+using TestManagement.API.Features.Environment.Get;
+
+namespace TestManagement.API.Services
+{
+    public class EnvironmentService
+    {
+        private readonly TestManagementDbContext _dbContext;
+
+        private readonly ILogger<EnvironmentService>? _logger = null;
+
+        public EnvironmentService(
+            TestManagementDbContext dbContext,
+            ILogger<EnvironmentService> logger
+            )
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        public async Task<ICollection<GetEnvironmentResponse>> GetAllAsync()
+        {
+            _logger?.LogDebug("EnvironmentService::GetAllAsync() start!");
+
+            var envionments = await _dbContext.Environments.ToListAsync();
+            var responses = new List<GetEnvironmentResponse>();
+            foreach (var environment in envionments )
+            {
+                var response = new GetEnvironmentResponse
+                {
+                    Name = environment.Name,
+                    Os = environment.Os ?? string.Empty,
+                    RunTime = environment.RunTime ?? string.Empty
+                };
+                responses.Add(response);
+            }
+
+            return responses;
+        }
+
+
+    }
+}
