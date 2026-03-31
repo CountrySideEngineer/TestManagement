d12b1be7379f3d2351f4beedf17ccaf84605b6d6
CountrySideEngineer <deep-sleeper@nifty.com>
2026-03-31 22:13:52 +0900

    Add UpdateEnvironmentRequest/Response classes.

---
+++ dev/src/TestManagement.API/TestManagement.API/Features/Environment/Update/UpdateEnvironmentRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TestManagement.API.Features.Environment.Update
+{
+    public class UpdateEnvironmentRequest
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Os { get; set; } = string.Empty;
+
+        public string RunTime { get; set; } = string.Empty;
+    }
+}
---
+++ dev/src/TestManagement.API/TestManagement.API/Features/Environment/Update/UpdateEnvironmentResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace TestManagement.API.Features.Environment.Update
+{
+    public class UpdateEnvironmentResponse
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Os { get; set; } = string.Empty;
+
+        public string RunTime { get; set; } = string.Empty;
+
+        public long VersionNumber { get; set; } = 0;
+    }
+}
