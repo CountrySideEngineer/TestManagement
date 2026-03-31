7b7af756d4f3425b2302545a30b5f0e1f572e36c
CountrySideEngineer <deep-sleeper@nifty.com>
2026-03-31 19:13:49 +0900

    Add Create/CreateEnvironmentRequest/Response class.

---
+++ dev/src/TestManagement.API/TestManagement.API/Features/Environment/Create/CreateEnvironmentRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TestManagement.API.Features.Environment.Create
+{
+    public class CreateEnvironmentRequest
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Os { get; set; } = string.Empty;
+
+        public string RunTime { get; set; } = string.Empty;
+    }
+}
---
+++ dev/src/TestManagement.API/TestManagement.API/Features/Environment/Create/CreateEnvironmentResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace TestManagement.API.Features.Environment.Create
+{
+    public class CreateEnvironmentResponse
+    {
+        public long Id { get; set; } = 0;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Os { get; set; } = string.Empty;
+
+        public string RunTime { get; set; } = string.Empty;
+
+        public long VersionNumber { get; set; } = 0;
+    }
+}
