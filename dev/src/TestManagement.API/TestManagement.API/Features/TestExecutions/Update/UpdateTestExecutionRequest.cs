64cc1e145116a9a30a8961d5673e11496f0c04c0
CountrySideEngineer <deep-sleeper@nifty.com>
2026-04-02 22:54:38 +0900

    Add UpdateTestExecutionRequest/Response classes.

---
+++ dev/src/TestManagement.API/TestManagement.API/Features/TestExecutions/Update/UpdateTestExecutionRequest.cs
@@ -0,0 +1,15 @@
+﻿using TestManagement.API.Features.testExecutions.Create;
+
+namespace TestManagement.API.Features.TestExecutions.Update
+{
+    public class UpdateTestExecutionRequest
+    {
+        public DateTime ExecutedAt { get; set; } = DateTime.UtcNow;
+
+        public string Environment { get; set; } = string.Empty;
+
+        public string Revision { get; set; } = string.Empty;
+
+        public List<TestCaseExecution> TestCases { get; set; } = new();
+    }
+}
---
+++ dev/src/TestManagement.API/TestManagement.API/Features/TestExecutions/Update/UpdateTestExecutionResponse.cs
@@ -0,0 +1,17 @@
+﻿using TestManagement.API.Features.testExecutions.Create;
+
+namespace TestManagement.API.Features.TestExecutions.Update
+{
+    public class UpdateTestExecutionResponse
+    {
+            public long TestExecutionId { get; set; } = 0;
+    
+            public DateTime ExecutedAt { get; set; } = DateTime.UtcNow;
+    
+            public string Environment { get; set; } = null!;
+    
+            public string Revision { get; set; } = string.Empty;
+    
+            public List< TestCaseExecution> TestCases { get; set; } = new();
+    }
+}
