66fd1fbd36bec65bb35b1023162eacbd998673ce
CountrySideEngineer <deep-sleeper@nifty.com>
2026-04-11 23:39:25 +0900

    Update TestExecutionService class. Add method to get text execution information and convert to view model object.

--- dev/src/TestManagement.APP/TestManagement.APP/Services/TestExecutionService.cs
+++ dev/src/TestManagement.APP/TestManagement.APP/Services/TestExecutionService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 using TestManagement.APP.ApiClients;
 using TestManagement.APP.Dto.TestExecution.Get;
+using TestManagement.APP.ViewModel.Executions;
 
 namespace TestManagement.APP.Services
 {
@@ -12,10 +13,39 @@ namespace TestManagement.APP.Services
 
         public TestExecutionService(
             ILogger<TestExecutionService> logger,
-        ITestExecutionApiClient apiClient)
+            ITestExecutionApiClient apiClient)
         {
             _logger = logger;
             _apiClient = apiClient;
+        }
+
+        public virtual async Task<IList<ExecutionIndexViewModel>> GetExecutionAsync()
+        {
+            _logger.LogInformation("TestExecutionService::GetExecutionAsync() start!");
+
+            ICollection<GetTestExecutionResponse>? testExecResponses = await _apiClient.GetTestExecutionsAsync();
+
+            var viewModels = new List<ExecutionIndexViewModel>();
+
+            if (testExecResponses != null)
+            {
+                foreach (var testExecResponse in testExecResponses)
+                {
+                    var viewModel = new ExecutionIndexViewModel
+                    {
+                        TestExecutionId = testExecResponse.TestExecutionId,
+                        ExecutedAt = testExecResponse.ExecutedAt,
+                        Environment = testExecResponse.Environment,
+                        Revision = testExecResponse.Revision,
+                        ErrorNum = testExecResponse.TestCases.Count(_ => _.IsFailed),
+                        SkippedNum = testExecResponse.TestCases.Count(_ => _.IsSkipped),
+                        ExcludedNum = testExecResponse.TestCases.Count(_ => _.IsExcluded),
+                        ExecutedNum = testExecResponse.TestCases.Count(_ => _.IsPassed || _.IsFailed)
+                    };
+                    viewModels.Add(viewModel);
+                }
+            }
+            return viewModels;
         }
 
         public virtual async Task<ICollection<GetTestExecutionResponse>?> GetTestExecutionsAsync()
