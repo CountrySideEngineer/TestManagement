7610ad5922aab95590cd28c19facb7dded33c51d
CountrySideEngineer <deep-sleeper@nifty.com>
2026-04-22 22:41:44 +0900

    Add GetEnvironmentResponse class.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Dto/Environment/Get/GetEnvironmentResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace TestManagement.APP.Dto.Environment.Get
+{
+    public class GetEnvironmentResponse
+    {
+        public long EnvironmentId { get; set; } = 0;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Os { get;set; } = string.Empty;
+
+        public string RunTime { get; set; } = string.Empty;
+
+        public long Version { get; set; } = 0;
+    }
+}
