cc64b0cf87af45d4fc16547297f47add907af85f
CountrySideEngineer <deep-sleeper@nifty.com>
2026-04-22 22:42:45 +0900

    Add new model and view model classes.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/ViewModel/Environment/EnvironmentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TestManagement.APP.ViewModel.Environment
+{
+    public class EnvironmentModel
+    {
+        public long EnvironmentId { get; set; } = 0;
+
+        public string DisplayName { get; set; } = string.Empty;
+
+        public EnvironmentModel() { }
+    }
+}
---
+++ dev/src/TestManagement.APP/TestManagement.APP/ViewModel/Executions/ExecutionCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using TestManagement.APP.ViewModel.Environment;
+
+namespace TestManagement.APP.ViewModel.Executions
+{
+    public class ExecutionCreateViewModel
+    {
+        public IList<EnvironmentModel> Environments { get; set; } = new List<EnvironmentModel>();
+
+        public ExecutionCreateViewModel() { }
+    }
+}
