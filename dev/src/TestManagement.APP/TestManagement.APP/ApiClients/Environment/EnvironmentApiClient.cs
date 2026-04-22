ba8405a41ba174b3dc095b983fe94e93bdd4c91c
CountrySideEngineer <deep-sleeper@nifty.com>
2026-04-22 22:33:09 +0900

    Add new api client class. Add "EnvironmentApiClient" class and its interface.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/ApiClients/Environment/EnvironmentApiClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+using TestManagement.APP.Dto.Environment.Get;
+
+namespace TestManagement.APP.ApiClients.Environment
+{
+    public class EnvironmentApiClient : IEnvironmentApiClient
+    {
+        private readonly ILogger<EnvironmentApiClient> _logger;
+
+        private readonly HttpClient _httpClient;
+
+        public EnvironmentApiClient(
+            ILogger<EnvironmentApiClient> logger,
+            IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClient = httpClientFactory.CreateClient("TestApiClient");
+        }
+
+        public async Task<IList<GetEnvironmentResponse>> GetEnvironmentsAsync()
+        {
+            _logger?.LogDebug("EnvironmentApiClient::GetEnvironmentsAsync() start!");
+
+            var result = await _httpClient!
+                .GetFromJsonAsync<List<GetEnvironmentResponse>>("api/environment") ?? 
+                    new List<GetEnvironmentResponse>();
+
+            return result;
+        }
+    }
+}
---
+++ dev/src/TestManagement.APP/TestManagement.APP/ApiClients/Environment/IEnvironmentApiClient.cs
@@ -0,0 +1,9 @@
+﻿using TestManagement.APP.Dto.Environment.Get;
+
+namespace TestManagement.APP.ApiClients.Environment
+{
+    public interface IEnvironmentApiClient
+    {
+        Task<IList<GetEnvironmentResponse>> GetEnvironmentsAsync();
+    }
+}
