3bbf030832a84792e2fd407cda2233fbac0db097
CountrySideEngineer <deep-sleeper@nifty.com>
2026-04-25 06:15:19 +0900

    Add EnvironmentService class and its interface.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Services/Environment/EnvironmentService.cs
@@ -0,0 +1,35 @@
+﻿using TestManagement.APP.ApiClients.Environment;
+using TestManagement.APP.Dto.Environment.Get;
+using TestManagement.APP.ViewModel.Environment;
+
+namespace TestManagement.APP.Services.Environment
+{
+    public class EnvironmentService : IEnvironmentService
+    {
+        private readonly ILogger<EnvironmentService> _logger;
+
+        private readonly IEnvironmentApiClient _apiClient;
+
+        public EnvironmentService(
+            ILogger<EnvironmentService> logger,
+            IEnvironmentApiClient apiClient)
+        {
+            _logger = logger;
+            _apiClient = apiClient;
+        }
+
+        public async Task<ICollection<EnvironmentModel>?> GetEnvironmentsAsync()
+        {
+            _logger.LogDebug("EnvironmentService::GetEnvironmentsAsync() start!");
+
+            IList<GetEnvironmentResponse> environmentResponses = await _apiClient.GetEnvironmentsAsync();
+            IList<EnvironmentModel> environmentViewModels = environmentResponses.Select(environmentResponse => new EnvironmentModel
+            {
+                EnvironmentId = environmentResponse.EnvironmentId,
+                DisplayName = environmentResponse.Name
+            }).ToList();
+
+            return environmentViewModels;
+        }
+    }
+}
---
+++ dev/src/TestManagement.APP/TestManagement.APP/Services/Environment/IEnvironmentService.cs
@@ -0,0 +1,10 @@
+﻿using TestManagement.APP.Dto.Environment.Get;
+using TestManagement.APP.ViewModel.Environment;
+
+namespace TestManagement.APP.Services.Environment
+{
+    public interface IEnvironmentService
+    {
+        Task<ICollection<EnvironmentModel>?> GetEnvironmentsAsync();
+    }
+}
