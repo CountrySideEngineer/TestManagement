1edff02885435cf3ee62c8ce0f5a9dc52a164dfe
CountrySideEngineer <deep-sleeper@nifty.com>
2026-05-26 22:04:03 +0900

    Add ITestResultSource interface. Add an new interface about test result source. It has interfaces to open and return stream object to access the test result content.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Interfaces/ITestResultSource.cs
@@ -0,0 +1,9 @@
+﻿namespace TestManagement.APP.Interfaces
+{
+    public interface ITestResultSource
+    {
+        Task<Stream> OpenAsync(string path);
+
+        Stream Open(string path);
+    }
+}
