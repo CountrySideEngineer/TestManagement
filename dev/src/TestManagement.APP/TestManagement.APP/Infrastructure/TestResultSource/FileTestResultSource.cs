97e0a023ce3bf4f86a5f410a2ae1b2b63a94ca9c
CountrySideEngineer <deep-sleeper@nifty.com>
2026-05-27 22:04:27 +0900

    Add FileTestResultSource class. Add a class to open path to test result file and return a stream to the test result.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Infrastructure/TestResultSource/FileTestResultSource.cs
@@ -0,0 +1,43 @@
+﻿using TestManagement.APP.Interfaces;
+
+namespace TestManagement.APP.Infrastructure.TestResultSource
+{
+    public class FileTestResultSource : ITestResultSource
+    {
+        public Stream Open(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"The file at path '{path}' was not found.");
+            }
+
+            Stream stream = new FileStream(
+                path,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.Read,
+                8192,
+                false);
+
+            return stream;
+        }
+
+        public Task<Stream> OpenAsync(string path, CancellationToken ct = default)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"The file at path '{path}' was not found.");
+            }
+
+            Stream stream = new FileStream(
+                path, 
+                FileMode.Open, 
+                FileAccess.Read, 
+                FileShare.Read, 
+                8192, 
+                true);
+
+            return Task.FromResult(stream);
+        }
+    }
+}
