b24800b7703291331dea2dccef5c5fa15bd29a38
CountrySideEngineer <deep-sleeper@nifty.com>
2026-05-27 22:03:06 +0900

    Added an interface for importing test results and its concrete implementation class.

---
+++ dev/src/TestManagement.APP/TestManagement.APP/Services/TestExecution/Import/IImportTestResultService.cs
@@ -0,0 +1,11 @@
+﻿using TestManagement.APP.Dto.TestResult.Import;
+
+namespace TestManagement.APP.Services.TestExecution.Import
+{
+    public interface IImportTestResultService
+    {
+        Task<ImportTestResultResponse> ImportTestResultAsync(
+            ImportTestResultRequest request, 
+            CancellationToken ct = default);
+    }
+}
---
+++ dev/src/TestManagement.APP/TestManagement.APP/Services/TestExecution/Import/ImportTestResultService.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+using TestManagement.APP.Dto.TestResult.Import;
+using TestManagement.APP.Services.TestExecution.Register;
+
+namespace TestManagement.APP.Services.TestExecution.Import
+{
+    public class ImportTestResultService : IImportTestResultService
+    {
+        private readonly ILogger<ImportTestResultService> _logger;
+
+        private readonly ISyncTestCasesService _syncTestCaseSerice;
+
+        private readonly IRegisterTestExecutionService _registerTestExecutionService;
+
+        public ImportTestResultService(
+            ISyncTestCasesService syncTestCaseSerice, 
+            IRegisterTestExecutionService registerTestExecutionService,
+            ILogger<ImportTestResultService> logger
+            )
+        {
+            _syncTestCaseSerice = syncTestCaseSerice
+                ?? throw new ArgumentNullException(nameof(syncTestCaseSerice));
+
+            _registerTestExecutionService = registerTestExecutionService
+                ?? throw new ArgumentNullException(nameof(registerTestExecutionService));
+
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public Task<ImportTestResultResponse> ImportTestResultAsync(ImportTestResultRequest request, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
