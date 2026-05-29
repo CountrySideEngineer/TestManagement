ffbaa08950b861a02b72616aa32cfed495665664
CountrySideEngineer <deep-sleeper@nifty.com>
2026-05-30 05:24:15 +0900

    Implement test case synchronization. - Added a method for synchronizing test cases to the ISyncTestCasesService interface. - Implemented the test case synchronization method in the SyncTestCasesService class, which implements the ISyncTestCasesService interface.

--- dev/src/TestManagement.APP/TestManagement.APP/Services/TestCase/ISyncTestCasesService.cs
+++ dev/src/TestManagement.APP/TestManagement.APP/Services/TestCase/ISyncTestCasesService.cs
@@ -5,5 +5,11 @@ namespace TestManagement.APP.Services.TestCase
 {
     public interface ISyncTestCasesService
     {
+        /// <summary>
+        /// Synchronizes test cases with the backend by sending a collection of <see cref="SyncTestCaseRequest"/> to the API.
+        /// </summary>
+        /// <param name="syncTestCaseRequests">The collection of test case requests to synchronize.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task SyncTestCasesAsync(IEnumerable<ParsedTestResult> syncTestCaseRequests);
     }
 }
--- dev/src/TestManagement.APP/TestManagement.APP/Services/TestCase/Sync/SyncTestCasesService.cs
+++ dev/src/TestManagement.APP/TestManagement.APP/Services/TestCase/Sync/SyncTestCasesService.cs
@@ -1,8 +1,43 @@
-﻿using TestManagement.APP.Dto.TestCase.Post;
+﻿using TestManagement.APP.ApiClients.TestCase;
+using TestManagement.APP.Dto.TestCase.Post;
+using TestManagement.APP.Dto.TestResult;
 
 namespace TestManagement.APP.Services.TestCase.Sync
 {
     public class SyncTestCasesService : ISyncTestCasesService
     {
+        private readonly ILogger<SyncTestCasesService> _logger;
+        private readonly ITestCaseSyncApiClient _apiClient;
+
+        public SyncTestCasesService(
+            ILogger<SyncTestCasesService> logger, 
+            ITestCaseSyncApiClient apiClient)
+        {
+            _logger = logger;
+            _apiClient = apiClient;
+        }
+
+        public async Task SyncTestCasesAsync(IEnumerable<ParsedTestResult> testResults)
+        {
+            _logger.LogDebug("SyncTestCasesService::SyncTestCasesAsync() start!");
+
+            var requests = new List<PostTestCaseRequest>();
+            foreach (var syncTestCaseRequest in testResults)
+            {
+                var request = new PostTestCaseRequest
+                {
+                    Code = syncTestCaseRequest.Code,
+                    Name = syncTestCaseRequest.Name,
+                    Description = syncTestCaseRequest.Description,
+                    TestLevelId = 1
+                };
+                requests.Add(request);
+
+            }
+
+            var result = await _apiClient.SyncAsync(requests);
+
+            return;
+        }
     }
 }
