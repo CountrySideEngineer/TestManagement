6c3d06bd786fb5ec824c5dd892a27fd9f6b1e1de
CountrySideEngineer <deep-sleeper@nifty.com>
2026-07-26 06:41:41 +0900

    Remove swagger from API project.

--- dev/src/TestManagement.API/TestManagement.API/Program.cs
+++ dev/src/TestManagement.API/TestManagement.API/Program.cs
@@ -25,15 +25,12 @@ builder.Services.AddMvc().AddXmlSerializerFormatters();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
