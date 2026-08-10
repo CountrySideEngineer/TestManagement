58a652db5edfa099f4bb52520ea555c65b7b6155
CountrySideEngineer <deep-sleeper@nifty.com>
2026-08-11 04:39:45 +0900

    Change name space of unit test.

--- dev/test/TestManagement.API/TestManagement.API.UTest.Controller.TestLevelController/TestLevelControllerTests.cs
+++ dev/test/TestManagement.API/TestManagement.API.UTest.Controller.TestLevelController/TestLevelControllerTests.cs
@@ -7,7 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging.Abstractions;
 
-namespace TestManagement.API.UTest.Controller.TestLevelController
+namespace TestManagement.API.Tests.Controller
 {
     public class TestLevelControllerTests
     {
