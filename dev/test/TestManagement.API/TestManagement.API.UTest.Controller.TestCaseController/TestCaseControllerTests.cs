f7db4832904d9c1bf9b0840a8d6234551ab9f4cc
CountrySideEngineer <deep-sleeper@nifty.com>
2026-08-12 05:30:20 +0900

    Add test code of "GetAllAsync" method.

--- dev/test/TestManagement.API/TestManagement.API.UTest.Controller.TestCaseController/TestCaseControllerTests.cs
+++ dev/test/TestManagement.API/TestManagement.API.UTest.Controller.TestCaseController/TestCaseControllerTests.cs
@@ -12,35 +12,10 @@ using Xunit;
 
 namespace TestManagement.API.Tests.Controller
 {
-    public class TestCaseControllerTests
+    public partial class TestCaseControllerTests
     {
         public TestCaseControllerTests()
         {
-        }
-
-        [Fact]
-        public async Task GetAllAsync_ReturnsOkWithCollection()
-        {
-            var mockService = new Mock<ITestCaseService>();
-            var mockLogger = new Mock<ILogger<TestCaseController>>();
-            var controller = new TestCaseController(mockLogger.Object, mockService.Object);
-
-            // Arrange
-            var list = new List<GetTestCaseResponse>
-            {
-                new GetTestCaseResponse { Code = "C1", Id = 1 },
-                new GetTestCaseResponse { Code = "C2", Id = 2 }
-            };
-            mockService.Setup(s => s.GetAllAsync(It.IsAny<CancellationToken>()))
-                        .ReturnsAsync((ICollection<GetTestCaseResponse>)list);
-
-            // Act
-            var result = await controller.GetAllAsync(CancellationToken.None);
-
-            // Assert
-            var ok = Assert.IsType<OkObjectResult>(result.Result);
-            var value = Assert.IsAssignableFrom<ICollection<GetTestCaseResponse>>(ok.Value);
-            Assert.Equal(2, value.Count);
         }
     }
 }
