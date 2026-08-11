2a32e30a748769e48ba7ae481955cb7c927d5efe
CountrySideEngineer <deep-sleeper@nifty.com>
2026-08-12 06:44:08 +0900

    Add unit test code of "GetByVersionIdAsync" method in TestCaseController class.

---
+++ dev/test/TestManagement.API/TestManagement.API.UTest.Controller.TestCaseController/TestCaseControllerTests_GetByVersionIdAsync.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TestManagement.API.Controllers;
+using TestManagement.API.Features.TestCases.Get;
+using TestManagement.API.Models;
+using TestManagement.API.Services;
+using Xunit;
+
+namespace TestManagement.API.Tests.Controller
+{
+    public partial class TestCaseControllerTests
+    {
+        [Fact]
+        public async Task GetByVersionIdAsync_WhenNotFound_ReturnsNotFound()
+        {
+            // Arrange
+            var mockService = new Mock<ITestCaseService>();
+            var mockLogger = new Mock<ILogger<TestCaseController>>();
+            var controller = new TestCaseController(mockLogger.Object, mockService.Object);
+
+            mockService.Setup(s => s.GetByVersionIdAsync(999, It.IsAny<CancellationToken>()))
+                        .ReturnsAsync((TestCaseVersion?)null);
+
+            // Act
+            var result = await controller.GetByVersionIdAsync(999, CancellationToken.None);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+    }
+}
